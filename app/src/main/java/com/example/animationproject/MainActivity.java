1a8e5a8cbbf020b1f69626a8d2a32405b12e7bf9
DenisFlow <deo9flow@gmail.com>
2021-01-08 22:35:22 +0300

    hi chalange

--- app/src/main/java/com/example/animationproject/MainActivity.java
+++ app/src/main/java/com/example/animationproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.animationproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -20,7 +21,18 @@ public class MainActivity extends AppCompatActivity {
         txtHelloWorld.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("HELLO", "some text");
+//                Log.i("HELLO", "some text");
+                txtHelloWorld.animate().alpha(0f).setDuration(1000);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtHelloWorld.setText("Hi World!");
+                        txtHelloWorld.animate().alpha(1f).setDuration(1000);
+                    }
+                }, 1000);
+
+
             }
         });
     }
