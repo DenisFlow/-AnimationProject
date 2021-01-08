f8a488e63274e5da76583b10e21b13830d627e09
DenisFlow <deo9flow@gmail.com>
2021-01-08 22:22:37 +0300

    first chalange

--- app/src/main/java/com/example/animationproject/MainActivity.java
+++ app/src/main/java/com/example/animationproject/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.animationproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txtHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHelloWorld = findViewById(R.id.textHelloWorld);
+
+        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("HELLO", "some text");
+            }
+        });
     }
 }
