6ee566d1891298ca8ab5d61ecf93b684d7967830
DenisFlow <deo9flow@gmail.com>
2021-01-09 14:57:59 +0300

    second chalange immage alpha

--- app/src/main/java/com/example/animationproject/MainActivity.java
+++ app/src/main/java/com/example/animationproject/MainActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txtHelloWorld;
+    private ImageView imgClick;
+    boolean bNowLeopard = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtHelloWorld = findViewById(R.id.textHelloWorld);
+        imgClick = findViewById(R.id.imageView);
 
         txtHelloWorld.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,13 +31,42 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        txtHelloWorld.setText("Hi World!");
+                        if (txtHelloWorld.getText() == "Hi World!"){
+                            txtHelloWorld.setText("Hello World!");
+                        }else{
+                            txtHelloWorld.setText("Hi World!");
+                        }
+
                         txtHelloWorld.animate().alpha(1f).setDuration(1000);
                     }
                 }, 1000);
 
+                    }
 
+        });
+
+        imgClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgClick.animate().alpha(0f).setDuration(1000);
+//                imgClick.animate().translationX(100).setDuration(1000);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (bNowLeopard){
+                            imgClick.setImageResource(R.drawable.lion);
+
+                        }else{
+                            imgClick.setImageResource(R.drawable.lepard);
+                        }
+                        bNowLeopard = !bNowLeopard;
+                        imgClick.animate().alpha(1f).setDuration(1000);
+//                        imgClick.animate().translationX(-100).setDuration(1000);
+                    }
+                }, 1000);
             }
+
         });
     }
 }
